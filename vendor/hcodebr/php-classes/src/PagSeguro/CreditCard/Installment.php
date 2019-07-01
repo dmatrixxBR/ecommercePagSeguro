ff727e19400641f72232de46e0fa69810ad4debe
dmatrixxBR <52224288+dmatrixxBR@users.noreply.github.com>
2019-07-01 18:22:14 -0300

    Classes PagSeguro

---
+++ vendor/hcodebr/php-classes/src/PagSeguro/CreditCard/Installment.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Hcode\PagSeguro\CreditCard;
+
+class Installment {
+
+    private $quantity;
+    private $value;
+}
---
+++ vendor/hcodebr/php-classes/src/PagSeguro/Sender.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Hcode\PagSeguro;
+
+class Sender {
+
+    private $name;
+    private $cpf;
+    private $bornDate;
+    private $phone;
+    private $email;
+    private $hash;
+    private $ip;
+    
+
+
+}
